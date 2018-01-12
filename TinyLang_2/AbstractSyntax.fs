eaeefe4275710fefd06eb5d793186651c39b41f6
William <wcrousse@users.noreply.github.com>
2018-01-12 12:04:41 -0800

    removes statements, expands expressions for ifs, and lets

--- TinyLang_2/AbstractSyntax.fs
+++ TinyLang_2/AbstractSyntax.fs
@@ -3,19 +3,12 @@
 module AbstractSyntax =
     type Id = string
 
-    type Statement =
-        | Definition of Id * Expression
-        | PrintStm of Expression
-        | CompoundStm of Statement * Statement
-        | IfStm of Expression * Statement
-        | IfElseStm of Expression * Statement * Statement
-        | WhileStm of Expression * Statement
-        | FunDefinition of Id * Id * Expression * Statement
-
     and Expression =
         | Id of Id
         | Num of int
-        | Prim of string * Expression * Expression
         | Bool of bool
-        | EseqExpr of Statement * Expression
+        | Prim of string * Expression * Expression
+        | IdDefinition of Id * Expression * Expression
+        | IfElseStm of Expression * Expression * Expression
+        | FunDefinition of Id * Id * Expression * Expression
         | Invoke of Id * Expression
--- TinyLang_2/Interpreter.fs
+++ TinyLang_2/Interpreter.fs
@@ -1,7 +1,6 @@
 ﻿module Interpreter
 open System.IO
 open TinyLang2.AbstractSyntax
-open Parse
 
 type 'a Env = (string * 'a) list
 type Closure = Id * Id * Expression * Value Env
@@ -14,45 +13,7 @@ let rec lookup id env =
         printf "kaboom"
         failwith <| sprintf "%s is not in scope" id
 
-let rec evalStatement (s : Statement) (env : Value Env ) : Value Env =
-    let scope = [];
-    match s with
-    | PrintStm(expr) ->
-        printfn "%d" <| evalExpression expr env
-        env
-    | Definition(id, e) ->
-       (id, Int(evalExpression e env)) :: env
-    | CompoundStm(stm1, stm2) ->
-        evalStatement stm1 env
-        |> evalStatement stm2
-    | IfStm (expr, stm) ->
-        match (evalExpression expr env) with
-            | 0 -> env
-            | _ ->
-                evalStatement stm env |> ignore
-                env
-    | IfElseStm (expr, ifPart, elsePart) ->
-        match (evalExpression expr env) with
-            | 0 ->
-                evalStatement elsePart env |> ignore
-                env
-            | _ ->
-                evalStatement ifPart env |> ignore
-                env
-    | WhileStm (expr, stm) ->
-        match (evalExpression expr env) with
-        | 0 -> env
-        | _ ->
-            let loopEnv = evalStatement stm env
-            evalStatement (WhileStm (expr, stm)) loopEnv |> ignore
-            env
-    | FunDefinition (funName, paramName, fbody, context) ->
-        printfn "Defining a function"
-        let closure = Closure(funName, paramName, fbody, env)
-        (funName, closure) :: env |>
-        evalStatement context
-
-and evalExpression (e : Expression) env : int =
+let rec evalExpression (e : Expression) env : int =
     match e with
     | Num n -> n
     | Bool b -> if b then 1 else 0
@@ -71,9 +32,7 @@ and evalExpression (e : Expression) env : int =
     | Id id ->  match lookup id env with
         | Int i -> i
         | _ -> failwith "bad times"
-    | EseqExpr(stm, expr) ->
-        evalStatement stm env
-        |> evalExpression expr
+
     | Invoke (funName, exprArgument) ->
         printfn "Invoking %s" funName
         let closure = lookup funName env
@@ -83,5 +42,3 @@ and evalExpression (e : Expression) env : int =
             let functionEnvironment = (param, argVal) :: (funName, closure) :: delarationEnvironment
             evalExpression bodyExpression functionEnvironment
         | _ -> failwith <| sprintf "%s is not a function" funName
-let tiny s = evalStatement(Parse.fromString s) []
-let tinyf f = evalStatement(Parse.fromFile f) []
